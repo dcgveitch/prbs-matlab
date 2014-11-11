1bb227f5f13d4bfe154940a4f72c4d644737aa24
David Veitch <david.veitch.10@ucl.ac.uk>
2014-11-11 11:46:35 +0000

    Tidy out files

--- P6_StatsCombined.m
+++ P6_StatsCombined.m
@@ -20,12 +20,12 @@ ind_1_Group=unique(r_seqLength);
 ind_2_Group=unique(r_seqPeriod);
 ind_3_Group=unique(r_nZones);
 
-mat_outP6.out_resultsCombined=cell(1,1,1,1,1,1,1,1,2);
-
 setup_batchSize=setup_nMC;
-setup_batchProc=50;
+setup_batchProc=10;
 setup_batchTrim=setup_nMC;
 d_batchRef=[];
+
+mat_outP6.out_resultsCombined=cell(1,1,1,1,1,1,1,1,2);
 
 for d_i=1:ceil(setup_nSim/setup_batchSize)
     for d_j=1:setup_batchTrim
@@ -42,6 +42,7 @@ for d_batch=1:ceil(length(d_batchRef)/setup_batchProc)
     d_batchSize=length(d_batchRun);
     
     rB_nZones=r_nZones(d_batchRun);
+    
     rB_seqLength=r_seqLength(d_batchRun);
     rB_seqPeriod=r_seqPeriod(d_batchRun);
     rB_seqMultiple=r_seqMultiple(d_batchRun);
@@ -58,6 +59,7 @@ for d_batch=1:ceil(length(d_batchRef)/setup_batchProc)
     d_batchList=[];
     
     for ref_bPerm = 1:d_batchSize
+        d_count=0;
         disp([' -Run ' num2str(ref_bPerm) '/' num2str(d_batchSize)]);
         
         clc_nZones=rB_nZones(ref_bPerm);
@@ -122,18 +124,11 @@ for d_batch=1:ceil(length(d_batchRef)/setup_batchProc)
                                         for d_flow2=1:d_nFlow2
                                             d_flowCount=(d_flow1-1)*d_nFlow2+d_flow2;                        
                                             d_flowProcessP=d_flowProcess{d_flow1,d_flow2}{d_tSeqA,d_nSeqA}{d_impulse,d_conc}';
-                                            d_resultsRaw=[];
 
                                             % Individual raw results with references
-                                            d_resultsRaw(1:d_nNoise,1)=d_batchRun(ref_bPerm);
-                                            d_resultsRaw(1:d_nNoise,2)=d_flowCount;
-                                            d_resultsRaw(1:d_nNoise,3)=d_dt;
-                                            d_resultsRaw(1:d_nNoise,4)=1:d_nNoise;
-                                            d_resultsRaw(1:d_nNoise,5)=d_flowProcessP(2,d_dt);
-                                            d_resultsRaw(1:d_nNoise,6)=d_flowProcessP(3:end,d_dt);
-                                            d_resultsRaw(1:d_nNoise,7)=(d_flowProcessP(3:end,d_dt)-d_flowProcessP(2,d_dt))/d_flowProcessP(2,d_dt);
-                                            d_resultsRaw(1:d_nNoise,8)=abs(d_flowProcessP(2,d_dt))/d_flowTotal(d_dt)/d_ndt/d_nNoise;
-                                            outB_resultsCombined{ind_1,ind_2,ind_3,d_solve,d_impulse,d_nSeqA,d_tSeqA,d_conc,d_flowType}(end+1:end+d_nNoise,1:8)=d_resultsRaw;
+                                            outB_resultsCombined{ind_1,ind_2,ind_3,d_solve,d_impulse,d_nSeqA,d_tSeqA,d_conc,d_flowType}{end+1,1}=[d_batchRun(ref_bPerm) d_flowCount d_dt d_flowProcessP(2,d_dt) abs(d_flowProcessP(2,d_dt))/d_flowTotal(d_dt)/d_ndt/d_nNoise]; 
+                                            outB_resultsCombined{ind_1,ind_2,ind_3,d_solve,d_impulse,d_nSeqA,d_tSeqA,d_conc,d_flowType}{end,2}=(d_flowProcessP(3:end,d_dt)-d_flowProcessP(2,d_dt))/d_flowProcessP(2,d_dt);
+                                            d_count=d_count+d_nNoise;
                                         end
                                     end
                                 end   
@@ -143,17 +138,24 @@ for d_batch=1:ceil(length(d_batchRef)/setup_batchProc)
                 end 
             end
         end
+        disp([' --Count ' num2str(d_count)]);
     end
     
-    d_bL=unique(d_batchList,'rows');
-    for d_i=1:size(d_bL)
-        try
-            d_in=cell2mat(mat_outP6.out_resultsCombined(d_bL(d_i,1), d_bL(d_i,2), d_bL(d_i,3), d_bL(d_i,4), d_bL(d_i,5), d_bL(d_i,6), d_bL(d_i,7), d_bL(d_i,8), d_bL(d_i,9)));
-            mat_outP6.out_resultsCombined(d_bL(d_i,1), d_bL(d_i,2), d_bL(d_i,3), d_bL(d_i,4), d_bL(d_i,5), d_bL(d_i,6), d_bL(d_i,7), d_bL(d_i,8), d_bL(d_i,9))={[d_in; outB_resultsCombined{d_bL(d_i,1), d_bL(d_i,2), d_bL(d_i,3), d_bL(d_i,4), d_bL(d_i,5), d_bL(d_i,6), d_bL(d_i,7), d_bL(d_i,8), d_bL(d_i,9)}]};
-        catch
-            mat_outP6.out_resultsCombined(d_bL(d_i,1), d_bL(d_i,2), d_bL(d_i,3), d_bL(d_i,4), d_bL(d_i,5), d_bL(d_i,6), d_bL(d_i,7), d_bL(d_i,8), d_bL(d_i,9))=outB_resultsCombined(d_bL(d_i,1), d_bL(d_i,2), d_bL(d_i,3), d_bL(d_i,4), d_bL(d_i,5), d_bL(d_i,6), d_bL(d_i,7), d_bL(d_i,8), d_bL(d_i,9));
+    if(~isempty(d_batchList))
+        d_bL=unique(d_batchList,'rows');
+        for d_i=1:size(d_bL,1)
+            filename=[num2str(d_bL(d_i,1)) '_' num2str(d_bL(d_i,2)) '_' num2str(d_bL(d_i,3)) '_' num2str(d_bL(d_i,4)) '_' num2str(d_bL(d_i,5)) '_' num2str(d_bL(d_i,6)) '_' num2str(d_bL(d_i,7)) '_' num2str(d_bL(d_i,8)) '_' num2str(d_bL(d_i,9))];
+            try  
+                load(filename);
+                out_results=[out_results; outB_resultsCombined{d_bL(d_i,1),d_bL(d_i,2),d_bL(d_i,3),d_bL(d_i,4),d_bL(d_i,5),d_bL(d_i,6),d_bL(d_i,7),d_bL(d_i,8),d_bL(d_i,9)}];
+            catch
+                out_results=outB_resultsCombined{d_bL(d_i,1),d_bL(d_i,2),d_bL(d_i,3),d_bL(d_i,4),d_bL(d_i,5),d_bL(d_i,6),d_bL(d_i,7),d_bL(d_i,8),d_bL(d_i,9)};
+            end
+            save(filename,'out_results','-v6');
+            clear out_results;
         end
     end
+    
     clear outB_* d_in;
 end
             
